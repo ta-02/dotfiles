43daa28ec91ab2a94bc1b814c7e4b5d72b2bee11
ta-02 <talal.gl2004@gmail.com>
2024-05-24 23:07:15 -0400

    changed some stuff

--- .config/nvim/lua/talz/plugins/substitute.lua
+++ .config/nvim/lua/talz/plugins/substitute.lua
@@ -1,17 +1,17 @@
 return {
-  "gbprod/substitute.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local substitute = require("substitute")
+	"gbprod/substitute.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local substitute = require("substitute")
 
-    substitute.setup()
+		substitute.setup()
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-    vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
-    vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
-  end,
+		keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
+		keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
+		keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
+		keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
+	end,
 }
