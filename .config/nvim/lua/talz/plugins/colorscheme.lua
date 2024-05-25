d738289146ca4fe4d7488f62c494b1ea65729238
ta-02 <talal.gl2004@gmail.com>
2024-05-24 21:28:04 -0400

    Deleted things that I did not use or need

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
 	"ribru17/bamboo.nvim",
+	"navarasu/onedark.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme bamboo")
+		vim.cmd("colorscheme onedark")
 	end,
 }
