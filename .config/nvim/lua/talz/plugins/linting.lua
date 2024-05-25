439a40cf85f75b0b21f6ba7000d44d5dcd0d970f
ta-02 <talal.gl2004@gmail.com>
2024-05-24 22:49:04 -0400

    changed

--- .config/nvim/lua/talz/plugins/linting.lua
+++ .config/nvim/lua/talz/plugins/linting.lua
@@ -7,8 +7,8 @@ return {
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
-			-- javascriptreact = { "eslint_d" },
-			-- typescriptreact = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
 			python = { "flake8" },
 		}
