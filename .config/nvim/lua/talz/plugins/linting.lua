cb45e10da8d778567777f5fa6c384f535ceba780
ta-02 <talal.gl2004@gmail.com>
2024-05-25 23:02:17 -0400

    changed things

--- .config/nvim/lua/talz/plugins/linting.lua
+++ .config/nvim/lua/talz/plugins/linting.lua
@@ -7,10 +7,10 @@ return {
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
-			-- javascriptreact = { "eslint_d" },
-			-- typescriptreact = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
-			python = { "flake8" },
+			python = { "pylint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
@@ -22,7 +22,7 @@ return {
 			end,
 		})
 
-		vim.keymap.set("n", "<leader>ll", function()
+		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
 		end, { desc = "Trigger linting for current file" })
 	end,
