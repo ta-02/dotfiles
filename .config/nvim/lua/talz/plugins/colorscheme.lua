675253d9e6b63869e20b48fe98484bf58faf03f1
ta-02 <talal.gl2004@gmail.com>
2024-05-27 00:59:21 -0400

    finished changed colors

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -9,7 +9,7 @@ return {
 			background = "hard",
 			--- How much of the background should be transparent. 2 will have more UI
 			--- components be transparent (e.g. status line background)
-			transparent_background_level = 100,
+			transparent_background_level = 0,
 			--- Whether italics should be used for keywords and more.
 			italics = false,
 			--- Disable italic fonts for comments. Comments are in italics by default, set
