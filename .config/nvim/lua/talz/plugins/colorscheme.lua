4619fb024113e58c357f4c7bf87bd43b87a3128e
ta-02 <talal.gl2004@gmail.com>
2024-05-28 21:57:03 -0400

    changed colors

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,67 +1,21 @@
 return {
-	"neanias/everforest-nvim",
+	"comfysage/evergarden",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("everforest").setup({
-			--- Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
-			--- Default is "medium".
-			background = "hard",
-			--- How much of the background should be transparent. 2 will have more UI
-			--- components be transparent (e.g. status line background)
-			transparent_background_level = 2,
-			--- Whether italics should be used for keywords and more.
-			italics = true,
-			--- Disable italic fonts for comments. Comments are in italics by default, set
-			--- this to `true` to make them _not_ italic!
-			disable_italic_comments = false,
-			--- By default, the colour of the sign column background is the same as the normal text
-			--- background, but you can use a grey background by setting this to `"grey"`.
-			sign_column_background = "none",
-			--- The contrast of line numbers, indent lines, etc. Options are `"high"` or
-			--- `"low"` (default).
-			ui_contrast = "low",
-			--- Dim inactive windows. Only works in Neovim. Can look a bit weird with Telescope.
-			---
-			--- When this option is used in conjunction with show_eob set to `false`, the
-			--- end of the buffer will only be hidden inside the active window. Inside
-			--- inactive windows, the end of buffer filler characters will be visible in
-			--- dimmed symbols. This is due to the way Vim and Neovim handle `EndOfBuffer`.
-			dim_inactive_windows = false,
-			--- Some plugins support highlighting error/warning/info/hint texts, by
-			--- default these texts are only underlined, but you can use this option to
-			--- also highlight the background of them.
-			diagnostic_text_highlight = false,
-			--- Which colour the diagnostic text should be. Options are `"grey"` or `"coloured"` (default)
-			diagnostic_virtual_text = "coloured",
-			--- Some plugins support highlighting error/warning/info/hint lines, but this
-			--- feature is disabled by default in this colour scheme.
-			diagnostic_line_highlight = false,
-			--- By default, this color scheme won't colour the foreground of |spell|, instead
-			--- colored under curls will be used. If you also want to colour the foreground,
-			--- set this option to `true`.
-			spell_foreground = false,
-			--- Whether to show the EndOfBuffer highlight.
-			show_eob = true,
-			--- Style used to make floating windows stand out from other windows. `"bright"`
-			--- makes the background of these windows lighter than |hl-Normal|, whereas
-			--- `"dim"` makes it darker.
-			---
-			--- Floating windows include for instance diagnostic pop-ups, scrollable
-			--- documentation windows from completion engines, overlay windows from
-			--- installers, etc.
-			---
-			--- NB: This is only significant for dark backgrounds as the light palettes
-			--- have the same colour for both values in the switch.
-			float_style = "bright",
-			--- Inlay hints are special markers that are displayed inline with the code to
-			--- provide you with additional information. You can use this option to customize
-			---
-			inlay_hints_background = "none",
-			on_highlights = function(highlight_groups, palette) end,
-			colours_override = function(palette) end,
+		require("evergarden").setup({
+			transparent_background = true,
+			contrast_dark = "medium", -- 'hard'|'medium'|'soft'
+			override_terminal = true,
+			style = {
+				tabline = { reverse = true, color = "green" },
+				search = { reverse = false, inc_reverse = true },
+				types = { italic = true },
+				keyword = { italic = true },
+				comment = { italic = true },
+			},
+			overrides = {}, -- add custom overrides
 		})
-
-		vim.cmd("colorscheme everforest")
+		vim.cmd("colorscheme evergarden")
 	end,
 }
--- .config/nvim/lua/talz/plugins/nvim-tree.lua
+++ .config/nvim/lua/talz/plugins/nvim-tree.lua
@@ -7,55 +7,33 @@ return {
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+		vim.opt.termguicolors = true
 
 		nvimtree.setup({
 			view = {
 				width = 35,
 				relativenumber = true,
 			},
-			-- change folder arrow icons
 			renderer = {
 				indent_markers = {
 					enable = true,
 				},
-				icons = {
-					glyphs = {
-						folder = {
-							arrow_closed = "", -- arrow when folder is closed
-							arrow_open = "", -- arrow when folder is open
-						},
-					},
-				},
 			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			filters = {
-				custom = { ".DS_Store" },
-			},
-			git = {
-				ignore = false,
-			},
+			actions = { open_file = { quit_on_open = true } },
+			filters = { dotfiles = false, custom = { "^.DS_Store$", "^\\.git$" } },
+			git = { enable = true, ignore = false, timeout = 500 },
 		})
-
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set(
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+		--
+		keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		-- keymap.set(
+		-- 	"n",
+		-- 	"<leader>e",
+		-- 	"<cmd>NvimTreeFindFileToggle<CR>",
+		-- 	{ desc = "Toggle file explorer on current file" }
+		-- ) -- toggle file explorer on current file
+		-- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+		-- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 	end,
 }
