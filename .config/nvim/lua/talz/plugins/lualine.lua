dbb31260c938db1aa2fee4eca63745bd3bb3e074
ta-02 <talal.gl2004@gmail.com>
2024-06-04 22:07:04 -0400

    chaged some things

--- .config/nvim/lua/talz/plugins/lualine.lua
+++ .config/nvim/lua/talz/plugins/lualine.lua
@@ -23,7 +23,7 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
-				lualine_b = { "branch", "diagnostics", "diff" },
+				lualine_b = { "branch", "diagnostics" },
 				lualine_c = {},
 				lualine_x = { "fileformat", "filetype" },
 				lualine_y = {},
