179e86e89703e09892e249e707048a00a06e9d8e
ta-02 <talal.gl2004@gmail.com>
2024-06-13 23:54:31 -0400

    changed some colours

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -31,6 +31,39 @@ return {
 		-- 	custom_colors = {}, -- Overwrite default colors
 		-- })
 		-- vim.cmd("colorscheme onenord")
+		local lackluster = require("lackluster")
+		local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
+		-- setup before set colorscheme
+		lackluster.setup({
+			-- You can overwrite the following syntax colors by setting them to one of...
+			--   1) a hexcode like "#a1b2c3" for a custom color
+			--   2) "default" or nil will just use whatever lackluster's default is.
+			tweak_syntax = {
+				string = "default",
+				-- string = "#a1b2c3", -- custom hexcode
+				-- string = color.green, -- lackluster color
+				string_escape = "default",
+				comment = "default",
+				builtin = "default", -- builtin modules and functions
+				type = "default",
+				keyword = "default",
+				keyword_return = "default",
+				keyword_exception = "default",
+			},
+			-- You can overwrite the following background colors by setting them to one of...
+			--   1) a hexcode like "#a1b2c3" for a custom color
+			--   2) "none" for transparency
+			--   3) "default" or nil will just use whatever lackluster's default is.
+			tweak_background = {
+				-- normal = "default", -- main background
+				normal = "none", -- transparent
+				-- normal = '#a1b2c3',    -- hexcode
+				-- normal = color.green,    -- lackluster color
+				telescope = "none", -- telescope
+				menu = "default", -- nvim_cmp, wildmenu ... (bad idea to transparent)
+				popup = "default", -- lazy, mason, whichkey ... (bad idea to transparent)
+			},
+		})
 		vim.cmd.colorscheme("lackluster-hack")
 	end,
 }
