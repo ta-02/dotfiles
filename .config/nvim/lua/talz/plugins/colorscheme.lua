be9e92cc3cab679ce3d7f72bb83259518bd2daa8
ta-02 <talal.gl2004@gmail.com>
2024-06-24 14:44:41 -0400

    fixed colors

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,68 +1,123 @@
 return {
-	"slugbyte/lackluster.nvim",
+	-- "slugbyte/lackluster.nvim",
+	"olivercederborg/poimandres.nvim",
+	-- "rose-pine/neovim",
+	-- name = "rose-pine",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- require("onenord").setup({
-		-- 	theme = "dark", -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
-		-- 	borders = true, -- Split window borders
-		-- 	fade_nc = false, -- Fade non-current windows, making them more distinguishable
-		-- 	-- Style that is applied to various groups: see `highlight-args` for options
-		-- 	styles = {
-		-- 		comments = "italic",
-		-- 		strings = "italic",
-		-- 		keywords = "italic",
-		-- 		functions = "NONE",
-		-- 		variables = "NONE",
-		-- 		diagnostics = "underline",
+		-- local lackluster = require("lackluster")
+		-- local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
+		-- -- setup before set colorscheme
+		-- lackluster.setup({
+		-- 	-- You can overwrite the following syntax colors by setting them to one of...
+		-- 	--   1) a hexcode like "#a1b2c3" for a custom color
+		-- 	--   2) "default" or nil will just use whatever lackluster's default is.
+		-- 	tweak_syntax = {
+		-- 		string = "default",
+		-- 		-- string = "#a1b2c3", -- custom hexcode
+		-- 		-- string = color.green, -- lackluster color
+		-- 		string_escape = "default",
+		-- 		comment = "default",
+		-- 		builtin = "default", -- builtin modules and functions
+		-- 		type = "default",
+		-- 		keyword = "default",
+		-- 		keyword_return = "default",
+		-- 		keyword_exception = "default",
 		-- 	},
-		-- 	disable = {
-		-- 		background = false, -- Disable setting the background color
-		-- 		float_background = true, -- Disable setting the background color for floating windows
-		-- 		cursorline = false, -- Disable the cursorline
-		-- 		eob_lines = true, -- Hide the end-of-buffer lines
+		-- 	-- You can overwrite the following background colors by setting them to one of...
+		-- 	--   1) a hexcode like "#a1b2c3" for a custom color
+		-- 	--   2) "none" for transparency
+		-- 	--   3) "default" or nil will just use whatever lackluster's default is.
+		-- 	tweak_background = {
+		-- 		-- normal = "default", -- main background
+		-- 		normal = "none", -- transparent
+		-- 		-- normal = '#a1b2c3',    -- hexcode
+		-- 		-- normal = color.green,    -- lackluster color
+		-- 		telescope = "none", -- telescope
+		-- 		menu = "default", -- nvim_cmp, wildmenu ... (bad idea to transparent)
+		-- 		popup = "default", -- lazy, mason, whichkey ... (bad idea to transparent)
 		-- 	},
-		-- 	-- Inverse highlight for different groups
-		-- 	inverse = {
-		-- 		match_paren = false,
-		-- 	},
-		-- 	custom_highlights = {}, -- Overwrite default highlight groups
-		-- 	custom_colors = {}, -- Overwrite default colors
 		-- })
-		-- vim.cmd("colorscheme onenord")
-		local lackluster = require("lackluster")
-		local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
-		-- setup before set colorscheme
-		lackluster.setup({
-			-- You can overwrite the following syntax colors by setting them to one of...
-			--   1) a hexcode like "#a1b2c3" for a custom color
-			--   2) "default" or nil will just use whatever lackluster's default is.
-			tweak_syntax = {
-				string = "default",
-				-- string = "#a1b2c3", -- custom hexcode
-				-- string = color.green, -- lackluster color
-				string_escape = "default",
-				comment = "default",
-				builtin = "default", -- builtin modules and functions
-				type = "default",
-				keyword = "default",
-				keyword_return = "default",
-				keyword_exception = "default",
-			},
-			-- You can overwrite the following background colors by setting them to one of...
-			--   1) a hexcode like "#a1b2c3" for a custom color
-			--   2) "none" for transparency
-			--   3) "default" or nil will just use whatever lackluster's default is.
-			tweak_background = {
-				-- normal = "default", -- main background
-				normal = "none", -- transparent
-				-- normal = '#a1b2c3',    -- hexcode
-				-- normal = color.green,    -- lackluster color
-				telescope = "none", -- telescope
-				menu = "default", -- nvim_cmp, wildmenu ... (bad idea to transparent)
-				popup = "default", -- lazy, mason, whichkey ... (bad idea to transparent)
-			},
+		-- vim.cmd.colorscheme("lackluster-hack")
+		-- require("rose-pine").setup({
+		-- 	variant = "auto", -- auto, main, moon, or dawn
+		-- 	dark_variant = "main", -- main, moon, or dawn
+		-- 	dim_inactive_windows = false,
+		-- 	extend_background_behind_borders = true,
+		--
+		-- 	enable = {
+		-- 		terminal = true,
+		-- 		legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+		-- 		migrations = true, -- Handle deprecated options automatically
+		-- 	},
+		--
+		-- 	styles = {
+		-- 		bold = false,
+		-- 		italic = false,
+		-- 		transparency = true,
+		-- 	},
+		--
+		-- 	groups = {
+		-- 		border = "muted",
+		-- 		link = "iris",
+		-- 		panel = "surface",
+		--
+		-- 		error = "love",
+		-- 		hint = "iris",
+		-- 		info = "foam",
+		-- 		note = "pine",
+		-- 		todo = "rose",
+		-- 		warn = "gold",
+		--
+		-- 		git_add = "foam",
+		-- 		git_change = "rose",
+		-- 		git_delete = "love",
+		-- 		git_dirty = "rose",
+		-- 		git_ignore = "muted",
+		-- 		git_merge = "iris",
+		-- 		git_rename = "pine",
+		-- 		git_stage = "iris",
+		-- 		git_text = "rose",
+		-- 		git_untracked = "subtle",
+		--
+		-- 		h1 = "iris",
+		-- 		h2 = "foam",
+		-- 		h3 = "rose",
+		-- 		h4 = "gold",
+		-- 		h5 = "pine",
+		-- 		h6 = "foam",
+		-- 	},
+		--
+		-- 	highlight_groups = {
+		-- 		-- Comment = { fg = "foam" },
+		-- 		-- VertSplit = { fg = "muted", bg = "muted" },
+		-- 	},
+		--
+		-- 	before_highlight = function(group, highlight, palette)
+		-- 		-- Disable all undercurls
+		-- 		-- if highlight.undercurl then
+		-- 		--     highlight.undercurl = false
+		-- 		-- end
+		-- 		--
+		-- 		-- Change palette colour
+		-- 		-- if highlight.fg == palette.pine then
+		-- 		--     highlight.fg = palette.foam
+		-- 		-- end
+		-- 	end,
+		-- })
+		--
+		-- vim.cmd("colorscheme rose-pine")
+		-- vim.cmd("colorscheme rose-pine-main")
+		-- vim.cmd("colorscheme rose-pine-moon")
+		-- vim.cmd("colorscheme rose-pine-dawn")
+		require("poimandres").setup({
+			bold_vert_split = false, -- use bold vertical separators
+			dim_nc_background = false, -- dim 'non-current' window backgrounds
+			disable_background = false, -- disable background
+			disable_float_background = false, -- disable background for floats
+			disable_italics = true, -- disable italics
 		})
-		vim.cmd.colorscheme("lackluster-hack")
+		vim.cmd("colorscheme poimandres")
 	end,
 }
--- .zshrc
+++ .zshrc
@@ -1,15 +1,3 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# fi
-# source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
-#
-# # -- To customize prompt, run `p10k configure` or edit ~/.p10k.zsh. -- 
-# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-#
-
 eval "$(starship init zsh)"
 
 # -- Use FD instead of FZF--
@@ -97,3 +85,6 @@ export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
 source ~/fzf-git.sh/fzf-git.sh
 source "$HOME/.zsh/plugins/zsh-system-clipboard/zsh-system-clipboard.zsh"
+
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
