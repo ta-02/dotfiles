ec03a8e02b287b4fe747a645257551b3e82d85d3
ta-02 <talal.gl2004@gmail.com>
2024-07-04 20:10:07 -0400

    removed rainbow lua

--- .config/nvim/lua/talz/plugins/linting.lua
+++ .config/nvim/lua/talz/plugins/linting.lua
@@ -5,8 +5,8 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			-- javascript = { "eslint_d" },
-			-- typescript = { "eslint_d" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
