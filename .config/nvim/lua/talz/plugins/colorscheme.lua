8b0b8d2731be153ff0e4b7afe4d02b605b79aff5
ta-02 <talal.gl2004@gmail.com>
2024-08-03 23:21:32 -0400

    changed some

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
-	"vague2k/vague.nvim",
-	-- "cdmill/neomodern.nvim",
+	-- "vague2k/vague.nvim",
+	"cdmill/neomodern.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		local transparent = true
-		-- require("neomodern").setup({
-		-- 	style = "roseprime",
-		-- 	transparent = transparent,
-		-- })
-		-- require("neomodern").load()
-		require("vague").setup({
+		require("neomodern").setup({
+			style = "roseprime",
 			transparent = transparent,
 		})
-		vim.cmd("colorscheme vague")
+		require("neomodern").load()
+		-- require("vague").setup({
+		-- 	transparent = transparent,
+		-- })
+		-- vim.cmd("colorscheme vague")
 	end,
 }
