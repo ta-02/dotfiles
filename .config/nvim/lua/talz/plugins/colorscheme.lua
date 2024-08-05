94f3bbcf1fc13ba71bbc111272641c54ddcc1a12
ta-02 <talal.gl2004@gmail.com>
2024-08-05 17:35:19 -0400

    changed some

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
 	-- "vague2k/vague.nvim",
 	"cdmill/neomodern.nvim",
+	-- "rose-pine/neovim",
+	-- name = "rose-pine",
 	lazy = false,
 	priority = 1000,
 	config = function()
@@ -14,5 +16,12 @@ return {
 		-- 	transparent = transparent,
 		-- })
 		-- vim.cmd("colorscheme vague")
+		-- require("rose-pine").setup({
+		-- 	styles = {
+		-- 		bold = false,
+		-- 		transparency = transparent,
+		-- 	},
+		-- })
+		-- vim.cmd("colorscheme rose-pine")
 	end,
 }
