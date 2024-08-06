5837479a01ce276c799956e3dbd69713038929f2
ta-02 <talal.gl2004@gmail.com>
2024-08-05 21:36:38 -0400

    final

--- .config/nvim/lua/talz/plugins/colorscheme.lua
+++ .config/nvim/lua/talz/plugins/colorscheme.lua
@@ -1,27 +1,27 @@
 return {
 	-- "vague2k/vague.nvim",
-	"cdmill/neomodern.nvim",
-	-- "rose-pine/neovim",
-	-- name = "rose-pine",
+	-- "cdmill/neomodern.nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	lazy = false,
 	priority = 1000,
 	config = function()
 		local transparent = true
-		require("neomodern").setup({
-			style = "roseprime",
-			transparent = transparent,
-		})
-		require("neomodern").load()
+		-- require("neomodern").setup({
+		-- 	style = "roseprime",
+		-- 	transparent = transparent,
+		-- })
+		-- require("neomodern").load()
 		-- require("vague").setup({
 		-- 	transparent = transparent,
 		-- })
 		-- vim.cmd("colorscheme vague")
-		-- require("rose-pine").setup({
-		-- 	styles = {
-		-- 		bold = false,
-		-- 		transparency = transparent,
-		-- 	},
-		-- })
-		-- vim.cmd("colorscheme rose-pine")
+		require("rose-pine").setup({
+			styles = {
+				bold = false,
+				transparency = transparent,
+			},
+		})
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
