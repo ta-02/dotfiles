7084891d42671078a7b6e8ed267e2e3df5730a6f
ta-02 <talal.gl2004@gmail.com>
2024-08-07 02:22:27 -0400

    added in harpoon

--- .config/nvim/lua/talz/plugins/bufferline.lua
+++ .config/nvim/lua/talz/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	opts = {},
-	config = function()
-		local bufferline = require("bufferline")
-		bufferline.setup({
-			options = {
-				style_preset = {
-					bufferline.style_preset.no_italic,
-					bufferline.style_preset.no_bold,
-				},
-				mode = "buffers",
-				separator_style = { " ", "" },
-				indicator = {
-					icon = "",
-					style = "none",
-				},
-				show_buffer_icons = false,
-				show_buffer_close_icons = false,
-				show_close_icon = false,
-			},
-		})
-	end,
+	-- "akinsho/bufferline.nvim",
+	-- version = "*",
+	-- opts = {},
+	-- config = function()
+	-- 	local bufferline = require("bufferline")
+	-- 	bufferline.setup({
+	-- 		options = {
+	-- 			style_preset = {
+	-- 				bufferline.style_preset.no_italic,
+	-- 				bufferline.style_preset.no_bold,
+	-- 			},
+	-- 			mode = "buffers",
+	-- 			separator_style = { " ", "" },
+	-- 			indicator = {
+	-- 				icon = "",
+	-- 				style = "none",
+	-- 			},
+	-- 			show_buffer_icons = false,
+	-- 			show_buffer_close_icons = false,
+	-- 			show_close_icon = false,
+	-- 		},
+	-- 	})
+	-- end,
 }
