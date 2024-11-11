fb11578efe2384fd0bd3420a70468753bd34f563
ta-02 <talal.gl2004@gmail.com>
2024-11-10 19:53:19 -0500

    updated config

--- .config/nvim/lua/talz/plugins/telescope.lua
+++ .config/nvim/lua/talz/plugins/telescope.lua
@@ -47,6 +47,7 @@ return {
 		local keymap = vim.keymap
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		keymap.set("n", "<leader>fd", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find open buffers" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
--- .config/nvim/lua/talz/plugins/which-key.lua
+++ .config/nvim/lua/talz/plugins/which-key.lua
@@ -3,6 +3,11 @@ return {
 	event = "VeryLazy",
 	init = function()
 		vim.o.timeout = true
+		require("which-key").setup({
+			icons = {
+				mappings = false,
+			},
+		})
 		vim.o.timeoutlen = 500
 	end,
 }
